b14d43a6f89052b848e6829416d58697597cb507
miljyface <pommybeloved.cc@gmail.com>
2025-10-27 23:20:37 +0800

    LOL KMS

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -4,8 +4,8 @@ mod metal_backend;
 #[cfg(all(target_os = "macos", feature = "metal-gpu"))]
 pub use metal_backend::MetalContext;
 
-#[cfg(all(not(target_os = "macos"), feature = "cuda"))]
+#[cfg(not(target_os = "macos"))]
 mod cuda_backend;
 
-#[cfg(all(not(target_os = "macos"), feature = "cuda"))]
+#[cfg(not(target_os = "macos"))]
 pub use cuda_backend::CudaContext;
