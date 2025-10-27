593afed2e0f517f950cf4b9bae4c85b2e1c25f90
miljyface <pommybeloved.cc@gmail.com>
2025-10-28 02:10:42 +0800

    im

--- src/gpu/cuda_backend.rs
+++ src/gpu/cuda_backend.rs
@@ -7,8 +7,18 @@ use std::ptr;
 macro_rules! cuda_check {
     ($expr:expr) => {{
         let status = $expr;
-        if status != cudaSuccess {
+        if status != cudaError::cudaSuccess {
             panic!("CUDA error at {}:{}: {:?}", file!(), line!(), status);
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! cublas_check {
+    ($expr:expr) => {{
+        let status = $expr;
+        if status != cublasStatus_t::CUBLAS_STATUS_SUCCESS {
+            panic!("cuBLAS error at {}:{}: {:?}", file!(), line!(), status);
         }
     }};
 }
@@ -28,7 +38,7 @@ impl CudaContext {
             cublas_check!(cublasCreate_v2(&mut handle));
             
             let max_elements = 16384 * 16384;
-            let bytes = (max_elements * std::mem::size_of::<f32>()) as u64;
+            let bytes = max_elements * std::mem::size_of::<f32>();
             
             let mut buffer_a = ptr::null_mut();
             let mut buffer_b = ptr::null_mut();
@@ -54,33 +64,43 @@ impl CudaContext {
         assert_eq!(k, k2);
         
         unsafe {
-            let a_bytes = (m * k * std::mem::size_of::<f32>()) as u64;
-            let b_bytes = (k * n * std::mem::size_of::<f32>()) as u64;
-            let c_bytes = (m * n * std::mem::size_of::<f32>()) as u64;
+            let a_bytes = m * k * std::mem::size_of::<f32>();
+            let b_bytes = k * n * std::mem::size_of::<f32>();
+            let c_bytes = m * n * std::mem::size_of::<f32>();
             
             cuda_check!(cudaMemcpy(
-                self.buffer_a as *mut _, a.as_ptr() as *const _, 
-                a_bytes, cudaMemcpyKind_cudaMemcpyHostToDevice
+                self.buffer_a as *mut _, 
+                a.as_ptr() as *const _, 
+                a_bytes, 
+                cudaMemcpyKind::cudaMemcpyHostToDevice
             ));
             cuda_check!(cudaMemcpy(
-                self.buffer_b as *mut _, b.as_ptr() as *const _, 
-                b_bytes, cudaMemcpyKind_cudaMemcpyHostToDevice
+                self.buffer_b as *mut _, 
+                b.as_ptr() as *const _, 
+                b_bytes, 
+                cudaMemcpyKind::cudaMemcpyHostToDevice
             ));
             
             let alpha = 1.0f32;
             let beta = 0.0f32;
             cublas_check!(cublasSgemm_v2(
-                self.handle, cublasOperation_t::CUBLAS_OP_N, cublasOperation_t::CUBLAS_OP_N,
-                n as i32, m as i32, k as i32, &alpha, 
+                self.handle, 
+                cublasOperation_t::CUBLAS_OP_N, 
+                cublasOperation_t::CUBLAS_OP_N,
+                n as i32, m as i32, k as i32, 
+                &alpha, 
                 self.buffer_b, n as i32,
-                self.buffer_a, k as i32, &beta, 
+                self.buffer_a, k as i32, 
+                &beta, 
                 self.buffer_c, n as i32
             ));
             
             let mut c = vec![0.0f32; m * n];
             cuda_check!(cudaMemcpy(
-                c.as_mut_ptr() as *mut _, self.buffer_c as *const _, 
-                c_bytes, cudaMemcpyKind_cudaMemcpyDeviceToHost
+                c.as_mut_ptr() as *mut _, 
+                self.buffer_c as *const _, 
+                c_bytes, 
+                cudaMemcpyKind::cudaMemcpyDeviceToHost
             ));
             cuda_check!(cudaDeviceSynchronize());
             
