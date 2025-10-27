821118c51caf198fcc9d63505f086cc8867a277e
miljyface <pommybeloved.cc@gmail.com>
2025-10-27 23:25:18 +0800

    LOL KMS

--- src/gpu/cuda_backend.rs
+++ src/gpu/cuda_backend.rs
@@ -1,13 +1,45 @@
-use cublas_sys::*;
-use cuda_sys::*;
+#[macro_export]
+macro_rules! cuda_check {
+    ($expr:expr) => {
+        {
+            let status = $expr;
+            if status != cuda_sys::CUresult::CUDA_SUCCESS {
+                panic!("CUDA error at {}:{}: {:?}", file!(), line!(), status);
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! cublas_check {
+    ($expr:expr) => {
+        {
+            let status = $expr;
+            if status != cublas_sys::cublasStatus_t::CUBLAS_STATUS_SUCCESS {
+                panic!("cuBLAS error at {}:{}: {:?}", file!(), line!(), status);
+            }
+        }
+    };
+}
+
+// --- Imports (bring all needed types/functions into scope) ---
+use cublas_sys::{
+    cublasHandle_t, cublasCreate_v2, cublasDestroy_v2, cublasSetStream_v2, 
+    cublasSgemm, cublasStatus_t, CUBLAS_OP_N
+};
+use cuda_sys::{
+    CUcontext, CUdevice, CUstream, CUdeviceptr, cuInit, cuDeviceGet, cuCtxCreate_v2, cuStreamCreate,
+    cuMemAlloc_v2, cuMemcpyHtoDAsync_v2, cuMemcpyDtoHAsync_v2, cuStreamSynchronize, cuStreamDestroy_v2,
+    cuMemFree_v2, cuCtxDestroy_v2
+};
 use std::ptr;
 use ndarray::Array2;
 
+// ---- CUDA Context struct ----
 pub struct CudaContext {
     pub ctx: CUcontext,
     pub handle: cublasHandle_t,
-    stream: CUstream,
-    // Pre-allocated buffers
+    pub stream: CUstream,
     buffer_a: CUdeviceptr,
     buffer_b: CUdeviceptr,
     buffer_c: CUdeviceptr,
@@ -19,32 +51,31 @@ impl CudaContext {
         unsafe {
             let mut cu_ctx: CUcontext = ptr::null_mut();
             let mut cu_dev: CUdevice = 0;
-            
-            cuda_check!(cuInit(0));
-            cuda_check!(cuDeviceGet(&mut cu_dev, 0));
-            cuda_check!(cuCtxCreate_v2(&mut cu_ctx, 0, cu_dev));
-            
+
+            crate::cuda_check!(cuInit(0));
+            crate::cuda_check!(cuDeviceGet(&mut cu_dev, 0));
+            crate::cuda_check!(cuCtxCreate_v2(&mut cu_ctx, 0, cu_dev));
+
             let mut handle: cublasHandle_t = ptr::null_mut();
-            cublas_check!(cublasCreate_v2(&mut handle));
-            
+            crate::cublas_check!(cublasCreate_v2(&mut handle));
+
             let mut stream = ptr::null_mut();
-            cuda_check!(cuStreamCreate(&mut stream, 0));
-            cublas_check!(cublasSetStream_v2(handle, stream));
-            
-            // Pre-allocate 16K x 16K buffers
+            crate::cuda_check!(cuStreamCreate(&mut stream, 0));
+            crate::cublas_check!(cublasSetStream_v2(handle, stream));
+
             let max_elements = 16384 * 16384;
             let bytes = max_elements * std::mem::size_of::<f32>();
+
             let mut buffer_a = 0;
             let mut buffer_b = 0;
             let mut buffer_c = 0;
-            
-            cuda_check!(cuMemAlloc_v2(&mut buffer_a, bytes));
-            cuda_check!(cuMemAlloc_v2(&mut buffer_b, bytes));
-            cuda_check!(cuMemAlloc_v2(&mut buffer_c, bytes));
-            
-            CudaContext { 
-                ctx: cu_ctx, 
-                handle, 
+            crate::cuda_check!(cuMemAlloc_v2(&mut buffer_a, bytes));
+            crate::cuda_check!(cuMemAlloc_v2(&mut buffer_b, bytes));
+            crate::cuda_check!(cuMemAlloc_v2(&mut buffer_c, bytes));
+
+            CudaContext {
+                ctx: cu_ctx,
+                handle,
                 stream,
                 buffer_a,
                 buffer_b,
@@ -53,7 +84,7 @@ impl CudaContext {
             }
         }
     }
-    
+
     pub fn matmul_f32(&self, a: &Array2<f32>, b: &Array2<f32>) -> Array2<f32> {
         let (m, k) = a.dim();
         let (k2, n) = b.dim();
@@ -116,6 +147,7 @@ impl CudaContext {
     }
 }
 
+// ---- Proper resource cleanup ----
 impl Drop for CudaContext {
     fn drop(&mut self) {
         unsafe {
@@ -127,17 +159,4 @@ impl Drop for CudaContext {
             cuCtxDestroy_v2(self.ctx);
         }
     }
-}
-
-// Add cublas error checking macro
-#[macro_export]
-macro_rules! cublas_check {
-    ($expr:expr) => {
-        {
-            let status = $expr;
-            if status != CUBLAS_STATUS_SUCCESS {
-                panic!("cuBLAS error at {}:{}: {:?}", file!(), line!(), status);
-            }
-        }
-    };
 }
